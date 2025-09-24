55f277b2022b0fe0005ede4b3156bfae9b9f3e2a
Lazar Andonov <lazar@andonov.biz>
2025-09-24 12:37:04 +0200

    Initial Commit

---
+++ VehicleCompanion/VehicleCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VehicleCompanionApp.swift
+//  VehicleCompanion
+//
+//  Created by Lazar Andonov on 24. 9. 2025..
+//
+
+import SwiftUI
+
+@main
+struct VehicleCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
